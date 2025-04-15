58448428b03c9077e2817d3e896c69cd30d255f3
joao <joaopedroq333@gmail.com>
2025-04-15 15:17:15 -0300

    Melhorias no código
    Melhorias Necessárias para garantir o funcionamento de ponta para o código

--- weight meter.py
+++ weight meter.py
@@ -1,31 +1,36 @@
-lista = []
-pessoas = []
-maior = menor = 0
-while True:
-    lista.append(str(input('nome: ')))
-    lista.append(int(input('peso: ')))
-    if len(pessoas) == 0:
-        maior = menor = lista[1]
-    else:
-        if lista[1] > maior:
-            maior = lista[1]
-        if lista[1] < menor:
-            menor = lista[1]
-    pessoas.append(lista[:])
-    lista.clear()
-    c = str(input('deseja continuar? [S/N] '))
+people = []
+person_data = []
+heaviest = lightest = 0
 
-    if c in 'Nn':
+while True:
+    person_data.append(str(input('Name: ')))
+    person_data.append(int(input('Weight: ')))
+
+    if len(people) == 0:
+        heaviest = lightest = person_data[1]
+    else:
+        if person_data[1] > heaviest:
+            heaviest = person_data[1]
+        if person_data[1] < lightest:
+            lightest = person_data[1]
+
+    people.append(person_data[:])  # Save a copy of current person
+    person_data.clear()
+
+    cont = str(input('Do you want to continue? [Y/N] '))
+    if cont.lower() == 'n':
         break
 
+print(f'\nTotal people registered: {len(people)}')
 
-print(f'foram cadastrados {len(pessoas)} pessoas até o momento')
-print(f'o maior peso registrado foi {maior}. peso de ', end='')
-for i in pessoas:
-    if i[1] == maior:
-        print(f'{i[0]}...',end='')
-print()
-print(f'o menor peso registrado foi {menor}. peso de ',end='')
-for i in pessoas:
-    if i[1] == menor:
-        print(f'{i[0]}...',end='')
+print(f'\nHeaviest weight recorded: {heaviest} kg. Person(s): ', end='')
+for person in people:
+    if person[1] == heaviest:
+        print(f'{person[0]}...', end='')
+
+print(f'\nLightest weight recorded: {lightest} kg. Person(s): ', end='')
+for person in people:
+    if person[1] == lightest:
+        print(f'{person[0]}...', end='')
+
+print('\nProgram ended.')
